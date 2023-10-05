6f5ff2abea10d0a5f3f5f8658f59c9a101067d4d
KananPy <kenan.semenderli@gmail.com>
2023-10-05 17:20:59 +0400

    client side

---
+++ feature/ClientShared/aboutContainer/abotText/AboutText.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AboutText = () => {
+  return (
+    <div className="flex-1 p-3">
+      <h1 className="text-center lg:text-left font-semibold text-2xl sm:text-3xl">About Us</h1>
+      <p className="mt-8 font-semibold text-center lg:text-left leading-10 text-[#828282] text-base md:text-lg">
+        Lorem ipsum is placeholder text commonly used in the graphic print and
+        publishing industries for previewing layouts and visual mockups.Lorem
+        ipsum is placeholder text commonly used in the graphic print and
+        publishing industries for previewing layouts and visual mockups.Lorem
+        ipsum is placeholder text commonly used in the graphic print and
+        publishing industries for previewing layouts and visual mockups.
+      </p>
+    </div>
+  );
+};
+
+export default AboutText;
