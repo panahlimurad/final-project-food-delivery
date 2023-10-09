ca74bbe1be8c133016a87f777fa92e2f5de44f52
panahlimurad <panahli.murad@gmail.com>
2023-10-09 14:16:01 +0400

    api

--- feature/adminShared/services/dataApi.js
+++ feature/adminShared/services/dataApi.js
@@ -10,6 +10,7 @@ token = userData?.user?.access_token
 }
 
 
+// Register
 export const Register = async (endpoint, data)=>{
     try {
         const response = await instanceAxiosAdmin.post(endpoint, data);
