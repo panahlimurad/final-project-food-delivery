eeae52c2b4233fd4ad6a871bc9ab3672c2c1981d
sabuhi <msebuhi040@gmail.com>
2023-10-17 11:50:53 +0400

    I edited userRouterPage branch

---
+++ pages/user/checkout/index.js
@@ -0,0 +1,18 @@
+import CheckoutPart from "../../../feature/ClientShared/components/CheckoutPart/CheckoutPart"
+import CheckoutYourOrder from "../../../feature/ClientShared/components/CheckoutYourOrder/CheckoutYourOrder"
+import LayoutUser from "../../../feature/ClientShared/components/LayoutUser/LayoutUser"
+import LayoutClient from "../../../layoutClient/LayoutClient"
+
+const Checkout = () => {
+    return (
+        <LayoutClient>
+            <LayoutUser>
+                <div className="flex">
+                    <CheckoutPart />
+                    <CheckoutYourOrder/>
+                </div>
+
+            </LayoutUser>
+        </LayoutClient>)
+}
+export default Checkout
--- pages/user/index.js
+++ pages/user/index.js
@@ -1,20 +1,12 @@
-import React from 'react'
-import SideBar from '../../feature/ClientShared/components/sideBar/SideBar'
-import CheckouSuccess from '../../feature/ClientShared/components/checkoutsuccess/CheckouSuccess'
-import CheckoutPart from '../../feature/ClientShared/components/CheckoutPart/CheckoutPart'
-import CheckoutYourOrder from '../../feature/ClientShared/components/CheckoutYourOrder/CheckoutYourOrder'
-import LayoutClient from '../../layoutClient/LayoutClient'
-const UserPage = () => {
-  return (
-    <LayoutClient>
-    <div className='flex md:justify-between p-3 justify-center  flex-wrap-reverse md:flex-nowrap '>
-      <SideBar/>
-      <CheckoutPart/>
-      <CheckoutYourOrder/>
-      {/* <CheckouSuccess/> */}
-      </div>
-    </LayoutClient>
-  )
+import LayoutUser from "../../feature/ClientShared/components/LayoutUser/LayoutUser"
+import LayoutClient from "../../layoutClient/LayoutClient"
+
+ const User = () => {
+    return (
+        <LayoutClient>
+            <LayoutUser />
+        </LayoutClient>
+    )
 }
 
-export default UserPage
+export default User
