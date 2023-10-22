ab1d3f80b660b15d39e7fc307300721aba46918f
sabuhi <msebuhi040@gmail.com>
2023-10-22 12:08:58 +0400

    Edited UserPage

--- feature/ClientShared/components/ProfilePart/ProfilePart.js
+++ feature/ClientShared/components/ProfilePart/ProfilePart.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import upload from '../../../public/svg/upload.svg'
+import upload from '../../../../public/svg/upload.svg'
 import Image from 'next/image'
 const ProfilePart = () => {
   return (
 <div className='flex w-full '>
-
-
-
-
-    <div className='w-[1031px]  mt-5   sm:px-8 py-12 sm:bg-[#F3F4F6] bg-[#FFFFFF]'>
+    <div className='w-[961px] sm:px-8 py-12 sm:bg-[#F3F4F6] bg-[#FFFFFF] max-[1200px]:w-full'>
 
 <h1 className='text-[#4F4F4F] text-[30px] h-8 font-semibold leading-6 sm:ml-0 ml-8 '>Profile</h1>
 <div className='flex justify-center flex-wrap items-center'>
@@ -28,7 +24,7 @@ const ProfilePart = () => {
       <input
         type="tel"
         id="inputField"
-        className="mt-1 sm:bg-[#FFFFFF] bg-[#F3F4F6] p-2 border text-[#828282] leading-6 font-normal rounded text-xl sm:w-[444px] h-[53px] w-[260px]  "
+        className="mt-1 sm:bg-[#FFFFFF] bg-[#F3F4F6] p-2 border text-[#828282] leading-6 font-normal rounded  text-lg sm:w-[424px] h-[53px] w-[260px]  "
         placeholder="+994"
         />
         </div>
@@ -42,7 +38,7 @@ const ProfilePart = () => {
        <input
         type="email"
         id="inputField"
-        className="mt-1 p-2 border sm:bg-[#FFFFFF] bg-[#F3F4F6] text-[#828282] leading-6 font-normal rounded text-xl w-[260px] h-[53px] sm:w-[444px] "
+        className="mt-1 p-2 border sm:bg-[#FFFFFF] bg-[#F3F4F6] text-[#828282] leading-6 font-normal rounded  text-lg w-[250px] h-[53px] sm:w-[424px] "
         placeholder="rahimlisarkhan@gmail.com"
         /> 
         </div>
@@ -57,7 +53,7 @@ const ProfilePart = () => {
       <input
       type="text"
       id="inputField"
-      className="mt-1 p-2 border sm:bg-[#FFFFFF] bg-[#F3F4F6] text-[#828282] leading-6 font-normal rounded text-xl w-[260px] h-[53px] sm:w-[444px] "
+      className="mt-1 p-2 border sm:bg-[#FFFFFF] bg-[#F3F4F6] text-[#828282] leading-6 font-normal rounded  text-lg w-[250px] h-[53px] sm:w-[424px] "
       placeholder="rahimlisarkhan"
       />
         </div>
@@ -71,7 +67,7 @@ const ProfilePart = () => {
        <input
     type="text"
     id="inputField"
-    className="mt-1 p-2 border sm:bg-[#FFFFFF] bg-[#F3F4F6] text-[#828282] leading-6 font-normal rounded text-xl w-[260px] h-[53px] sm:w-[444px] "
+    className="mt-1 p-2 border sm:bg-[#FFFFFF] bg-[#F3F4F6] text-[#828282] leading-6 font-normal rounded  text-lg w-[250px] h-[53px] sm:w-[424px] "
     placeholder="Ataturk 45 Ganclik Baku"
     />
     </div>
@@ -85,14 +81,14 @@ const ProfilePart = () => {
        <input
     type="text"
     id="inputField"
-    className="mt-1 p-2 border sm:bg-[#FFFFFF] bg-[#F3F4F6] text-[#828282] leading-6 font-normal rounded text-xl  w-[260px] sm:w-[444px] h-[53px]  "
+    className="mt-1 p-2 border sm:bg-[#FFFFFF] bg-[#F3F4F6] text-[#828282] leading-6 font-normal rounded  text-lg  w-[250px] sm:w-[424px] h-[53px]  "
     placeholder="Sarkhan Rahimli " 
     />
     </div>
 
     <div>
 
-      <button className='bg-[#6FCF97]  w-[260px]  sm:w-[444px] h-[53px] mt-7 rounded text-[#FFFFFF] text-[18px] font-semibold leading-6' >Save</button>
+      <button className='bg-[#6FCF97]  w-[250px]  sm:w-[424px] h-[53px] mt-7 rounded text-[#FFFFFF] text-[18px] font-semibold leading-6' >Save</button>
     </div>
     </div>
 
--- pages/user/basket/index.js
+++ pages/user/basket/index.js
@@ -16,26 +16,19 @@ const Basket = () => {
                         <table className="w-full h-full">
                             <thead>
                                 <tr>
-                                    <th colSpan={3} className={`${styles.tableRow} relative  border-b-[1px] border-[#E0E0E0]`} > <h5 className="pt-4 text-3xl">Your Basket</h5> <div className=" flex  items-center ml-4"><Image src={add} /><span className="flex gap-4 ml-4">1</span>items</div></th>
+                                    <th colSpan={3} className={`${styles.tableRow} relative  border-b-[1px] border-[#E0E0E0]`} > <h5 className="pt-4 text-3xl max-[420px]:text-lg">Your Basket</h5> <div className=" flex  items-center ml-4 max-[420px]:hidden"><Image src={add} /><span className="flex gap-4 ml-4">1</span>items</div></th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr className={`${styles.tableRow} relative  border-b-[1px] border-[#E0E0E0]`}>
-                                    <td className="w-[68px] pr-8  pl-8 max-[981px]:pr-4 max-[380px]:pr-2 max-[380px]:w-[40px]"><div className="w-[68px] h-[68px] max-[683px]:w-[48px] max-[683px]:h-[48px] max-[380px]:w-[38px] max-[380px]:h-[38px] "><Image src={pizza} alt="pizza" /></div></td>
+                                    <td className="w-[68px] pr-8  pl-8 max-[981px]:pr-4 max-[380px]:pr-2 max-[380px]:w-[40px] max-[380px]:pl-2 "><div className="w-[68px] h-[68px] max-[683px]:w-[48px] max-[683px]:h-[48px] max-[380px]:w-[38px] max-[380px]:h-[38px] "><Image src={pizza} alt="pizza" /></div></td>
                                     <td className="w-9/12  max-[981px]:w-8/12  max-[922px]:w-3/5 max-[380px]:w-7/12 max-[328px]:w-3/5"><div><h3 className={`${styles.restaurantName}`}>Papa John’s Pizza Restaurant</h3> <p className={`${styles["restaurant-paragraf"]}`}>$15.80</p></div></td>
                                     <td className="w-[40px] p-2"> <button className={`${styles["basket-btn"]} `}>
                                         <span >+</span>2<span>-</span>
                                     </button></td>
                                     <td><span className='w-[25px] h-[25px] absolute right-2 top-6'><Image src={deleteItem} alt='delete' /></span></td>
                                 </tr>
-                                <tr className={`${styles.tableRow} relative  border-b-[1px] border-[#E0E0E0]`}>
-                                    <td className="w-[68px] pr-8  pl-8 max-[981px]:pr-4 max-[380px]:pr-2 max-[380px]:w-[40px]"><div className="w-[68px] h-[68px] max-[683px]:w-[48px] max-[683px]:h-[48px] max-[380px]:w-[38px] max-[380px]:h-[38px] "><Image src={pizza} alt="pizza" /></div></td>
-                                    <td className="w-9/12  max-[981px]:w-8/12  max-[922px]:w-3/5 max-[380px]:w-7/12 max-[328px]:w-3/5"><div><h3 className={`${styles.restaurantName}`}>Papa John’s Pizza Restaurant</h3> <p className={`${styles["restaurant-paragraf"]}`}>$15.80</p></div></td>
-                                    <td className="w-[40px] p-2"> <button className={`${styles["basket-btn"]} `}>
-                                        <span >+</span>2<span>-</span>
-                                    </button></td>
-                                    <td><span className='w-[25px] h-[25px] absolute right-2 top-6'><Image src={deleteItem} alt='delete' /></span></td>
-                                </tr>
+                              
                             </tbody>
                         </table>
                     </ScrollBarContainer>
