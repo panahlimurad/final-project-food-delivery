6bbcd4da50dcbf2380e90109200a77aeaf506367
panahlimurad <panahli.murad@gmail.com>
2023-10-24 20:55:01 +0400

    createClientAuth

---
+++ feature/ClientShared/components/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const LoaderClient = () => {
+  return (
+    <div className='w-[50%] h-full flex items-center justify-center'>
+       <div className='w-20 h-20 rounded-full text-white shadow bg-[#eb5757] flex items-center justify-center'>
+       Loading...
+       </div>
+    </div>
+  )
+}
+
+export default LoaderClient
