bbac6c21c84a69ccf42c20fcff1fd4002fed0da5
KananPy <kenan.semenderli@gmail.com>
2023-10-25 11:32:19 +0400

    delete items

--- feature/adminShared/components/Select/index.js
+++ feature/adminShared/components/Select/index.js
@@ -1,12 +1,12 @@
-import React  from "react";
+import React from "react";
 import { BsChevronDown } from "react-icons/bs";
 import { useQuery } from "react-query";
 import { GetCategory } from "../../services/dataApi";
-
-
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { setCategory } from "../../../../redux/features/productDetails/productSlice";
 
 const Select = () => {
-
   const { data, isLoading, isError, error } = useQuery(
     "category",
     GetCategory,
@@ -16,17 +16,26 @@ const Select = () => {
       },
     }
   );
+
+  const dispatch = useDispatch();
+
+  const selectedCategory = useSelector((state) => state.category);
+
+  const handleCategoryChange = (event) => {
+    const selectedCategory = event.target.value;
+    dispatch(setCategory(selectedCategory));
+    console.log("seletecedCategory", selectedCategory);
+  };
   const dataArray = data ? Object.values(data) : [];
 
   return (
     <div className="flex relative">
-      <select class="md:w-[200px] w-full h-9 text-[#F2F2F2] sm:rounded-2xl rounded-md tracking-wide appearance-none bg-[#5A5B70] bg-no-repeat bg-[96%] md:bg-[162px_4px] cursor-pointer px-3 bg-grayInput outline-none ">
+      <select onChange={handleCategoryChange} value={selectedCategory} class="md:w-[200px] w-full h-9 text-[#F2F2F2] sm:rounded-2xl rounded-md tracking-wide appearance-none bg-[#5A5B70] bg-no-repeat bg-[96%] md:bg-[162px_4px] cursor-pointer px-3 bg-grayInput outline-none ">
         {dataArray[1]?.data?.map((item, index) => (
-          <option value={item.id} className="bg-white text-black h-4">
-          {item.name}
-        </option>
+          <option  className="bg-white text-black h-4">
+            {item.name}
+          </option>
         ))}
-      
       </select>
       <BsChevronDown className="text-white absolute right-4 top-3 cursor-pointer" />
     </div>
