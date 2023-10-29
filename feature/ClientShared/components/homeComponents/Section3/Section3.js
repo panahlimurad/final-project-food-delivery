5dca123bd07152f8205ae8242a61bc5cf03a542b
sabuhi <msebuhi040@gmail.com>
2023-10-30 00:33:19 +0400

    change

--- feature/ClientShared/components/homeComponents/Section3/Section3.js
+++ feature/ClientShared/components/homeComponents/Section3/Section3.js
@@ -1,20 +1,19 @@
 import Image from "next/image";
 import styles from "./Section3.module.css";
 
-export const Section3 = ({ data }) => {
-    const offers = data;
+export const Section3 = () => {
     return (
         <div className="mb-44">
             <div className={`flex justify-around items-center mb-40 ${styles["section-3"]}`}>
                 <div className={`w-2/5 ${styles["top-container"]}`}>
-                    <h3 className={`${styles["section3-title"]}`}>{offers[0]?.name}</h3>
-                    <p className={`${styles["section3-paragraf"]}`}>{offers[0]?.description}</p>
+                    <h3 className={`${styles["section3-title"]}`}>.name</h3>
+                    <p className={`${styles["section3-paragraf"]}`}>description</p>
                 </div>
                 <div className={`w-1/3 relative mt-8 ${styles["bottom-container"]}`}>
                     <div className={`${styles["section3-container"]}`}></div>
                     <div className={`w-[626px] h-[441px] absolute z-10 top-6 -right-20 ${styles["section3-image1"]}`}>
                         <Image
-                            src={offers[0]?.img_url}
+                            src=""
                             alt="kfc"
                             width={498}
                             height={472}
@@ -28,7 +27,7 @@ export const Section3 = ({ data }) => {
                     <div className={`${styles["section3-container1"]} ml-12`}></div>
                     <div className={`w-[468.362px] h-[472.84px] absolute z-10 top-6 -right-12 ${styles["section3-image2"]}`}>
                         <Image
-                            src={offers[1]?.img_url}
+                            src=""
                             alt="pizza"
                             width={468.362}
                             height={472.84}
@@ -37,20 +36,20 @@ export const Section3 = ({ data }) => {
                     </div>
                 </div>
                 <div className={`w-2/5 mb-6 ${styles["top-container"]}`}>
-                    <h3 className={`${styles["section3-title"]}`}>{offers[1]?.name}</h3>
-                    <p className={`${styles["section3-paragraf"]}`}>{offers[1]?.description}</p>
+                    <h3 className={`${styles["section3-title"]}`}>.name</h3>
+                    <p className={`${styles["section3-paragraf"]}`}>description</p>
                 </div>
             </div>
             <div className={`flex justify-around items-center ${styles["section-3"]}`}>
                 <div className={`w-2/5 ${styles["top-container"]}`}>
-                    <h3 className={`${styles["section3-title"]}`}>{offers[2]?.name}</h3>
-                    <p className={`${styles["section3-paragraf"]}`}>{offers[2]?.description}</p>
+                    <h3 className={`${styles["section3-title"]}`}>name</h3>
+                    <p className={`${styles["section3-paragraf"]}`}>description</p>
                 </div>
                 <div className={`w-1/3 relative mt-16 ${styles["bottom-container"]}`}>
                     <div className={`${styles["section3-container"]}`}></div>
                     <div className={`w-[637px] h-[461px] absolute z-10 top-6 -right-6 ${styles["section3-image3"]}`}>
                         <Image
-                            src={offers[2]?.img_url}
+                            src=""
                             alt="fryPoteto"
                             width={637}
                             height={461}
