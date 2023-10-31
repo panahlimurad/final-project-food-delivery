40c2cff9ab4c76dabe017210fb86c9c558954503
panahlimurad <panahli.murad@gmail.com>
2023-10-31 11:46:42 +0400

    updateUser

--- feature/adminShared/services/dataApi.js
+++ feature/adminShared/services/dataApi.js
@@ -1,81 +1,88 @@
 import { instanceAxiosAdmin } from "../helper/instanceAxiosAdmin";
 
-// let token = null;
+export const Register = async (endpoint, data) => {
+  try {
+    const response = await instanceAxiosAdmin.post(endpoint, data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-if (typeof window !== "undefined") {
-const userJSONData = localStorage.getItem("userData")
-const userData = JSON.parse(userJSONData)
-token = userData?.user?.access_token
+export const Login = async (endpoint, data) => {
+  try {
+    const response = await instanceAxiosAdmin.post(endpoint, data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-// }
+export const PostCategory = async (data) => {
+  try {
+    const response = await instanceAxiosAdmin.post("/api/category", data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-// console.log("token", token);
+export const PostProduct = async (data) => {
+  try {
+    const response = await instanceAxiosAdmin.post("/api/products", data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const Register = async (endpoint, data)=>{
-    try {
-        const response = await instanceAxiosAdmin.post(endpoint, data);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
+export const PutUser = async (data) => {
+  const userJSONData = localStorage.getItem("clientData");
+  const userData = JSON.parse(userJSONData);
+  const token = userData?.user?.access_token;
+  try {
+    const response = await instanceAxiosAdmin.put("/api/auth/user", data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const Login = async (endpoint, data)=>{
-    try {
-        const response = await instanceAxiosAdmin.post(endpoint, data);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
+export const PostOffer = async (data) => {
+  try {
+    const response = await instanceAxiosAdmin.post("/api/offer", data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const PostCategory = async (data)=>{
-    try {
-        const response = await instanceAxiosAdmin.post("/api/category", data);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
+export const PostImg = async (data) => {
+  try {
+    const response = await instanceAxiosAdmin.post("/api/uploads", data, {
+      headers: {
+        "Content-Type": "multi-part",
+      },
+    });
 
-export const PostProduct = async (data)=>{
-    try {
-        const response = await instanceAxiosAdmin.post("/api/products", data);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const PostOffer = async (data)=>{
-    try {
-        const response = await instanceAxiosAdmin.post("/api/offer", data);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
-
-export const PostImg = async (data)=>{
-    try {
-        const response = await instanceAxiosAdmin.post("/api/uploads", data ,{ headers: {
-                   "Content-Type": "multi-part",
-                 }});
-        
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
-
-export const PostRestaurants = async (data)=>{
-    try {
-        const response = await instanceAxiosAdmin.post("/api/restuarants", data);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
+export const PostRestaurants = async (data) => {
+  try {
+    const response = await instanceAxiosAdmin.post("/api/restuarants", data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const DeleteRestaurants = async (id) => {
   try {
@@ -95,61 +102,78 @@ export const DeleteProduct = async (id) => {
   }
 };
 
-export const PostBasket = async (data)=>{
-    try {
-        const response = await instanceAxiosAdmin.post("/api/basket/add", data);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
+export const PostBasket = async (data) => {
+  try {
+    const response = await instanceAxiosAdmin.post("/api/basket/add", data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const GetRestaurants = async ()=>{
-    try {
-        const response = await instanceAxiosAdmin.get("/api/restuarants");
-        return response.data;
-    }catch(error){
-        throw error
-    }
-}
+export const GetRestaurants = async () => {
+  try {
+    const response = await instanceAxiosAdmin.get("/api/restuarants");
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const GetProducts = async ()=>{
-    try {
-        const response = await instanceAxiosAdmin.get("/api/products");
-        return response.data;
-    }catch(error){
-        throw error
-    }
-}
+export const GetProducts = async () => {
+  try {
+    const response = await instanceAxiosAdmin.get("/api/products");
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const GetCategory = async ()=>{
-    try {
-        const response = await instanceAxiosAdmin.get("/api/category");
-        return response.data;
-    }catch(error){
-        throw error
-    }
-}
+export const GetCategory = async () => {
+  try {
+    const response = await instanceAxiosAdmin.get("/api/category");
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const GetOffer = async ()=>{
-    try {
-        const response = await instanceAxiosAdmin.get("/api/offer");
-        return response.data;
-    }catch(error){
-        throw error
-    }
-}
+export const GetUser = async () => {
+  const userJSONData = localStorage.getItem("clientData");
+  const userData = JSON.parse(userJSONData);
+  const token = userData?.user?.access_token;
+  try {
+    const response = await instanceAxiosAdmin.get("/api/auth/user", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const GetOffer = async () => {
+  try {
+    const response = await instanceAxiosAdmin.get("/api/offer");
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 // DONT WORK YET
-export const GetBasket = async (endpoint)=>{
-    try {
-        const response = await instanceAxiosAdmin.get(endpoint, {
-            headers:{
-                "Authorization":`Bearer +${token}`
-            }
-        });
-        return response.data;
-    }catch(error){
-        throw error
-    }
-}
+export const GetBasket = async (endpoint) => {
+  try {
+    const response = await instanceAxiosAdmin.get(endpoint, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
