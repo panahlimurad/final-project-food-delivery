5a713401682687a9a152a725a9601e56eba4e65f
KananPy <kenan.semenderli@gmail.com>
2023-11-18 01:35:03 +0400

    data gets by id

--- feature/ClientShared/components/CardRestaurant/CardRestaurant.js
+++ feature/ClientShared/components/CardRestaurant/CardRestaurant.js
@@ -3,47 +3,52 @@ import Image from "next/image";
 import { useQuery } from "react-query";
 import { GetRestaurantsById } from "../../../adminShared/services/dataApi";
 
-const CardRestaurant = ({name, img, cuisine, delivery_min,category_id , delivery_price, item_id}) => {
+
+const CardRestaurant = ({item_id}) => {
   // console.log("name", name);
 
-  console.log('testing')
-  // const { data, isLoading, isError, error } = useQuery(
-  //   ['restaurant', item_id], 
-  //   async () => {
-  //     try {
-  //       return await GetRestaurantsById(item_id);
-  //     } catch (error) {
-  //       throw error;
-  //     }
-  //   }
-  // );
 
-  // console.log("data from id", data);
-  
+  const { data, isLoading, isError, error } = useQuery(
+    ['restaurant', item_id],
+    async () => {
+      try {
+        return await GetRestaurantsById(item_id);
+      } catch (error) {
+        throw error;
+      }
+    }
+  );
+
+  console.log("rest id", item_id);
+
+  const dataArray = data ? Object.values(data) : [];
+
+  console.log("checkin client data", dataArray);
+
   return (
     <div
 
       className="w-[235px] h-[345px] flex flex-col gap-4 justify-center items-center p-3 cursor-pointer"
       style={{
         boxShadow: "2px 4px 4px 4px rgba(0, 0, 0, 0.25)",
-        
+
       }}>
       <div className=" w-[80%] h-[80%]">
-       <Image 
+       <Image
        className="w-[100%] h-[100%]"
-       src={img}
+       src={dataArray[1]?.data?.img_url}
        width={100}
        height={100}
        alt="Res"/>
       </div>
-      <div className="font-bold text-xl text-[#4F4F4F]">{name}</div>
+      <div className="font-bold text-xl text-[#4F4F4F]">{dataArray[1]?.data?.name}</div>
       <div className="text-base font-normal text-[#828282]">
-        <p>{cuisine}</p>
+        <p>{dataArray[1]?.data?.cuisine}</p>
       </div>
       <div className="flex justify-between w-[200px]">
-        <div className="font-bold text-base text-[#4F4F4F]">{delivery_price} Delivery</div>
+        <div className="font-bold text-base text-[#4F4F4F]">{dataArray[1]?.data?.delivery_price} Delivery</div>
         <div className="w-[78px] h-[30px] flex items-center justify-center rounded-3xl font-medium text-base text-white bg-[#D63626]">
-        {delivery_min} Min
+        {dataArray[1]?.data?.delivery_min} Min
         </div>
       </div>
     </div>
--- feature/ClientShared/components/CardRestaurantContainer/CardRestaurantContainer.js
+++ feature/ClientShared/components/CardRestaurantContainer/CardRestaurantContainer.js
@@ -12,42 +12,36 @@ export default function CardRestaurantContainer() {
     "restaurant",
     GetRestaurants,
   );
-  
-  
+
+
   // console.log("datass", data);
 
   const dataArray = data ? Object.values(data) : [];
-  
+
   const selectedReataurants = useSelector((state) => state.category);
   const filteredRestaurants = selectedReataurants
     ? dataArray[1]?.data?.filter(
         (restaurant) => restaurant.category_id === selectedReataurants
-      
+
       )
     : dataArray[1]?.data;
   // console.log("filteredTestaurantss", filteredRestaurants);
   const { push } = useRouter();
   return (
 
-      <div className="flex flex-wrap justify-center gap-7 mt-20 md:mt-4"> 
+      <div className="flex flex-wrap justify-center gap-7 mt-20 md:mt-4">
         {filteredRestaurants?.map((restaurant,index) => (
           <button   onClick={() => push(ROUTER.RESTUARANTS_ID(restaurant.id))}>
              <CardRestaurant
           key={restaurant.name}
-          name={restaurant.name}
-          delivery_price={restaurant.delivery_price}
-          category_id={restaurant.category_id }
-          delivery_min={restaurant.delivery_min}
-          img={restaurant.img_url}
           item_id={restaurant.id}
-          cuisine={restaurant.cuisine}
-          category={restaurant.category_id}
+
         />
           </button>
-        
-        ))} 
-      </div> 
-    
-    
+
+        ))}
+      </div>
+
+
   );
 }
